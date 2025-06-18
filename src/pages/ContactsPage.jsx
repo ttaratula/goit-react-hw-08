699e0ca441ac81bb6653820dedf5622188068562
Tetiana Taratula <ttaratula@gmail.com>
2025-06-18 20:09:45 +0100

    fix

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../components/ContactForm/ContactForm.jsx";
-import SearchBox from "../components/SearchBox/SearchBox";
+import SearchBox from "../components/SearchBox/SearchBox.jsx";
 import ContactList from "../components/ContactList/ContactList.jsx";
 import EditContactForm from "../components/EditContactForm/EditContactForm.jsx";
 
